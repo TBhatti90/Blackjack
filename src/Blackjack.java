d0bb31f3f1a6b9ed8c05b6636d92ce3a3f23d57c
Taaha Bhatti <94648546+TBhatti90@users.noreply.github.com>
2025-05-01 20:52:08 -0400

    Checked for correct input for number of players. Added shuffling of deck. Begin working on adding cards to players' hands, and control flow.

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Blackjack {
 	public class Player {
 		private double money;
 		private double bet;
-		private ArrayList<Character> hand;
+		private ArrayList<String> hand;
 
 		public Player() {
 			money = 10000;
@@ -29,24 +32,73 @@ public class Blackjack {
 		public void setBet(double bet) {
 			this.bet = bet;
 		}
-		public ArrayList<Character> getHand() {
-			return null;
+		public ArrayList<String> getHand() {
+			return new ArrayList<>(hand);
 		}
 	}
 
 	ArrayList<Player> players;
 	Player dealer;
-	ArrayList<Character> deck;
+	ArrayList<String> deck;
+	ArrayList<String> discard;
 
-	public Blackjack() {
+	public Blackjack(int size) {
 		dealer = new Player("dealer");
-		players = new ArrayList<Player>(1);
-		deck = new ArrayList<Character>(312);
+		players = new ArrayList<>(size);
+		deck = new ArrayList<>(312);
+		discard = new ArrayList<>();
+		for (int i = 1; i < 14; ++i) {
+			for (int j = 0; j < 24; ++j) {
+				switch (i) {
+					case 1:
+						deck.add("Ace");
+						break;
+					case 11:
+						deck.add("Jack");
+						break;
+					case 12:
+						deck.add("Queen");
+						break;
+					case 13:
+						deck.add("King");
+						break;
+					default:
+						deck.add(Integer.toString(i));
+						break;
+				}
+			}
+		}
+		Collections.shuffle(deck);
 	}
 
+	public ArrayList<String> getDeck() {
+		return new ArrayList<>(deck);
+	}
 
+	public void setDeck(ArrayList<String> deck) {
+		this.deck = deck;
+	}
 
 	public static void main(String[] args) {
-		Blackjack game = new Blackjack();
+		int size = 0;
+		System.out.println("Hi, welcome to Blackjack, how many players are playing? (Max of 7)");
+		Scanner input = new Scanner(System.in);
+		do {
+			try {
+				size = input.nextInt();
+			} catch (InputMismatchException e) {
+				System.err.println("Error, input must be a natural number, between 1 and 7.");
+				input.nextLine();
+			}
+		} while (size < 1 || size > 7);
+		System.out.println();
+		Blackjack game = new Blackjack(size);
+		ArrayList<String> currentDeck = game.getDeck();
+		currentDeck.forEach(System.out::println);
+
+
+
+
+		input.close();
 	}
 }
